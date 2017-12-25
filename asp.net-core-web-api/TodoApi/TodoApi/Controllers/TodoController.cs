a841e5f8b5ce3c8570afd071ff01d23257bd4dbe
LeeTrent <Lee.Trent.1@gmail.com>
2017-12-25 09:19:18 -0600

    Adds 'TodoApi', a 'asp.net-core-web-api' Microsoft tutorial

---
+++ asp.net-core-web-api/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using System.Linq;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        /****************************************************************
+         * GET /api/todo
+         * A successful response is 200 (OK)
+         ****************************************************************/
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        /****************************************************************
+         * GET /api/todo/{id}
+         * A successful response is 200 (OK)
+         ****************************************************************/
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        /****************************************************************
+         * POST /api/todo
+         * A successful response is 201 (Created)
+         ****************************************************************/
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        /****************************************************************
+         * PUT /api/todo/{id}
+         **************************************************************** 
+         * Update is similar to Create, but uses HTTP PUT.
+         * The response is 204 (No Content).
+         * According to the HTTP spec,* a PUT request requires the client
+         * to send the entire updated entity, not just the deltas.
+         * To support partial updates, use HTTP PATCH.
+         ****************************************************************/
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsComplete = item.IsComplete;
+            todo.Name = item.Name;
+
+            _context.TodoItems.Update(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        /****************************************************************
+         * DELETE /api/todo/{id}
+         * The response is 204 (No Content).
+         ****************************************************************/
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
